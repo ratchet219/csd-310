7f42e010bf50aac44b1160fb016fb49ad2ac7aa6
ratchet219 <95461098+ratchet219@users.noreply.github.com>
2022-01-23 16:20:51 -0600

    Update pytech_delte.py

--- pytech_delte.py
+++ pytech_delte.py
@@ -1,1 +1,15 @@
+from pymongo import MongoClient
 
+url="mongodb+srv://admin:admin@cluster0.3chly.mongodb.net/test";
+
+conn = MongoClient(url)
+
+db = conn.pytech
+
+collection = db.students
+
+cursor = collection.find()
+
+for record in cursor:
+
+print(record)
